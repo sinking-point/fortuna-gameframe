53d0a8b3638fff03c04e8531f1c4ddb0d06402b6
Billy Bradley <billy.jack.bradley@gmail.com>
2018-05-01 10:25:23 +0100

    started run_frame()

--- gameframe.c
+++ gameframe.c
@@ -7,5 +7,14 @@ void set_scene(game_scene *next) {
 }
 
 int run_frame(int status) {
-  
+  for(int i = 0; i < scene->num_entities; i++) {
+    game_entity *current = entities + i;
+
+    int prev_x = current->x;
+    int prev_y = current->y;
+
+    current->update();
+
+    
+  }
 }
