5b159281416f1775ccccf2ac23e5b23fd972f790
Billy Bradley <billy.jack.bradley@gmail.com>
2018-05-01 14:40:38 +0100

    fleshed out gameframe.c, may be nearly ready to test

--- gameframe.c
+++ gameframe.c
@@ -7,14 +7,48 @@ void set_scene(game_scene *next) {
 }
 
 int run_frame(int status) {
+  game_entity[scene->num_entities] prev_states;
+  game_scene prev_scene;
+  prev_scene.num_entities = scene->num_entities;
+  prev_scene.entities = prev_states;
+  
   for(int i = 0; i < scene->num_entities; i++) {
     game_entity *current = entities + i;
 
-    int prev_x = current->x;
-    int prev_y = current->y;
+    prev_states[i] = *current;
 
     current->update();
-
-    
   }
+
+  for(int i = 0; i < scene->num_entities; i++) {
+    if(scene->entities[i] != prev_states.entities[i]) {
+      
+    }
+  }
+}
+
+void update_block(game_scene prev_scene, int x, int y, int width, int height) {
+  for(int i = x; i < x + width; i++) {
+    for(int j = y; j < y + height; j++) {
+      colour new = get_colour(*scene, i, j);
+
+      if(new != get_colour(prev_scene, i, j)) {
+        push_pixel(i, j, new);
+      }
+    }
+  }
+}
+
+colour get_colour(game_scene to_paint, int x, int y) {
+  for(int i = to_paint.num_entities - 1; i >= 0; i--) {
+    if(contains(to_paint.entities[i], x, y)) {
+      colour c = to_paint.entities[i].pixel_colour(x - to_paint.entities[i].x, y - to_paint.entities[i].y);
+
+      if(c != to_paint.entities[i].transparency_code) {
+        return c;
+      }
+    }
+  }
+
+  return 0;
 }
